286a1c7a059f61c0464687a2ed6e325d07720488
repoproject <repo_user@163.com>
2015-01-05 17:05:28 +0800

    增加图文消息，文本汉字，文本链接demo，wechat4j-1.0.2

---
+++ src/com/sample/wechat/Hello.java
@@ -0,0 +1,41 @@
+package com.sample.wechat;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Hello
+ */
+public class Hello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Hello() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String result = "你好，wechat4j";
+		response.setHeader("content-type", "text/html;charset=UTF-8");
+		response.getOutputStream().write(result.getBytes());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String result = "你好，wechat4j";
+		response.setHeader("content-type", "text/html;charset=UTF-8");
+		response.getOutputStream().write(result.getBytes());
+	}
+
+}
--- src/com/sample/wechat/MyWechat.java
+++ src/com/sample/wechat/MyWechat.java
@@ -2,9 +2,12 @@ package com.sample.wechat;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.log4j.Logger;
 import org.sword.wechat4j.WechatSupport;
 
 public class MyWechat extends WechatSupport {
+	
+	private static Logger logger = Logger.getLogger(MyWechat.class);
 
 
 	public MyWechat(HttpServletRequest request) {
@@ -43,7 +46,22 @@ public class MyWechat extends WechatSupport {
 
 	@Override
 	protected void onText() {
-		responseText("hello world!");
+		String content = super.wechatRequest.getContent().trim();
+		logger.info(content);
+		//文本测试
+		if(content.equals("1")){
+			responseText("你好，hello world!<a href=\"http://www.baidu.com\">这是链接</a>");
+		}
+		else if(content.equals("2")){
+			responseNew("图文消息", "测试图文消息", "http://upload.qqfuzhi.com/portal/showimg.php?img=e2dnYyk8PHEhIys9Y3t8Z3w9YGd8YXY9YmI9cHx%2BPHtnZ2NMen50f3xydz1wdHosPGFmYX8nTHEuJ3Z2IXFyJnUiICAqcnAnInYhcHJ2InAnKndycidwKyAgdnIqdiN1KitxdyojIysjcSAiJipyK3YqIXd1JCt1JyBxKnIkcCt1JyYkKysicCAjIiokKyogcHd1ICAhcXArciUjI3EhdyYjKiIncSclIiUqJyAkInEgKiV2IiEiJnEgKyp2cXV3cCEmJ3EjcHYqJHIrdytwIyYgIHIicHAgcXFwIiIldyIhNXIuISMrNXEuISMr", 
+					"http://www.chengn.com");
+		}
+		else{
+			responseText("你好，你的输入为 " + content + "\n"
+					+ "请按照如下操作输入:\n"
+					+ "1 文本\n"
+					+ "2 图文\n");
+		}
 	}
 
 	@Override
@@ -74,6 +92,12 @@ public class MyWechat extends WechatSupport {
 	protected void view() {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	protected void templateMsgCallback() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
